79ee291742a4ba00624e1367d985f31e631413a5
TomCool-TomTom <tom.cool@tomtom.com>
2026-04-15 06:04:22 +0200

    feat(4021): Carousel index indicator

--- apps/web/src/components/deck-flashcards.tsx
+++ apps/web/src/components/deck-flashcards.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { EmblaCarouselType } from 'embla-carousel';
 import { Carousel } from '@mantine/carousel';
 import { Card } from "@cortexa/types";
-import { AspectRatio, Progress } from '@mantine/core';
+import { AspectRatio, Badge, Group, Progress, Text } from '@mantine/core';
 import { FlashCard } from "@cortexa/ui";
 import { throttle } from "@cortexa/utils";
 
@@ -17,6 +17,7 @@ export function DeckFlashCards({ cards }: DeckCardsProps) {
 
     const [scrollProgress, setScrollProgress] = useState(0);
     const [embla, setEmbla] = useState<EmblaCarouselType | null>(null);
+    const [currentSlide, setCurrentSlide] = useState(0);
 
 
     // Throttle scroll progress updates to every 100ms
@@ -33,23 +34,44 @@ export function DeckFlashCards({ cards }: DeckCardsProps) {
         
     }, [embla, throttledSetScrollProgress]);
 
+    const handleSelect = useCallback(() => {
+        if (!embla) {
+            return;
+        }
+        setCurrentSlide(embla.selectedScrollSnap());
+    }, [embla]);
+
     useEffect(() => {
         if (!embla) {
             return;
         }
+        // bind events
         embla.on('scroll', handleScroll);
+        embla.on('select', handleSelect);
+        // Initialize values
         handleScroll();
-    }, [embla]);
+        handleSelect();
+        return () => {
+            embla.off('scroll', handleScroll);
+            embla.off('select', handleSelect);
+        };
+    }, [embla, handleScroll]);
     
     return (
         <>
+            <Group justify='space-between'>
+                <Text size="sm" c='dimmed' mb='xs'>
+                    Click on a card to flip it
+                </Text>
+                <Badge size="md" mb='xs' variant='default'>
+                    {currentSlide + 1} / {cards.length}
+                </Badge>
+            </Group>
             <Carousel
-                emblaOptions={{ dragFree: false }}
                 slideSize="99.9%"
                 slideGap="lg"
                 getEmblaApi={setEmbla}
                 initialSlide={0}
-                draggable={false}
             >
                 {cards.map((card) => (
                     <AspectRatio key={card.id} component={Carousel.Slide} ratio={16/9}>
